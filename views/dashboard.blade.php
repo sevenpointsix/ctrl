6d1f0656a62c6a75adce83779b070d4bd6b30f3a
Chris Gibson <chris@sevenpointsix.com>
2016-05-07 09:36:11 +0100

    testig vendor... is it pushing

--- views/dashboard.blade.php
+++ views/dashboard.blade.php
@@ -1,5 +1,8 @@
 @extends('ctrl::master')
 
+@section('js')
+<script src="{{ asset('assets/vendor/ctrl/assets/vendor/corejs-typeahead/typeahead.bundle.min.js') }}"></script>
+@stop
 
 @section('content')
 	
@@ -8,47 +11,41 @@
 	</div>
 
 	<div class="row">
+
 		<div class="col-md-6">
-
 			
-			 <?php /* I didn't like this approach: it just duplicates the menu
-				<div class="btn-group btn-group-vertical" role="group">
-				{{-- Currently we put everything in a "Content" group but that's not right, we should allow "floating" items with no category
-				  <button type="button" class="btn btn-default">1</button>
-				  <button type="button" class="btn btn-default">2</button>
-				 --}}
-				 @foreach ($menu_links as $menu_title=>$links)
-
-				  <div class="btn-group" role="group">	
-				    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				      {{ $menu_title }}
-				      <span class="caret"></span>
-				    </button>
-				    <ul class="dropdown-menu">
-				      	@foreach ($links as $link)
-							<li><a href="{{ route('ctrl::list_objects',$link['id']) }}">{{ $link['title'] }}</a></li>
-						@endforeach
-				    </ul>
-				  </div>
-				  @endforeach
-				</div>
-			*/ ?>
-		
+				<h4>Search for an existing item...</h4>
+				<form>
+				  <div class="form-group">
+				    <label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
+				    <div class="input-group">
+				      <div class="input-group-addon"><i class="fa fa-search"></i></div>
+				      <input type="text" class="form-control" id="exampleInputAmount" placeholder="Search">			      
+				    </div>
+				  </div>			  
+				</form>
+				<h4>... or choose from the list below.</h4>
+				
+				<div class="row">
 				@foreach ($menu_links as $menu_title=>$links)
 
-					
-					<div class="list-group">
-						@if (count($links) > 1) 
-					  	<div class="list-group-item list-group-item-info"><strong>{{ $menu_title }}</strong></div>					  
-					  	@endif
-					  @foreach ($links as $link)
-						<a href="{{ route('ctrl::list_objects',$link['id']) }}" class="list-group-item">
-							{{-- Not keen on this <span class="badge">14</span> --}}
-							{{ $link['title'] }}
-						</a>
-						@endforeach
+					<div class="col-md-6">
+						<div class="list-group">
+							@if (count($links) > 1) 
+						  	<div class="list-group-item list-group-item-info"><strong>{{ $menu_title }}</strong></div>					  
+						  	@endif
+						  @foreach ($links as $link)
+							<a href="{{ route('ctrl::list_objects',$link['id']) }}" class="list-group-item">							
+								{{-- Not keen on this <span class="badge">14</span> --}}
+								{{-- I would like to add an "add" button here though. How? --}}							
+								<button class="btn btn-xs btn-success pull-right"><i class="fa fa-plus" onclick="document.location = '{{ route('ctrl::edit_object',$link['id']) }}'; return false"></i></button>
+								{{ $link['title'] }}
+							</a>
+							
+
+							@endforeach
+						</div>
 					</div>
-				
 				   
 				  @endforeach
 
