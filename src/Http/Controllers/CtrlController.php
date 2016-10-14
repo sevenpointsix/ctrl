285bb081113550f2b547b5f54aaa091a7fb5c94e
Chris Gibson <chris@phoenixdigital.agency>
2016-10-14 12:42:47 +0100

    removnig debugbar properly

--- src/Http/Controllers/CtrlController.php
+++ src/Http/Controllers/CtrlController.php
@@ -22,7 +22,6 @@ use Storage;
 use Log;
 use Schema;
 
-use Barryvdh\Debugbar; // Will this kick off if we've not installed DebugBar on the main site? We need it so that we can disable it when returning Froala AJAX 
 use Datatables;
 use Maatwebsite\Excel\Facades\Excel;
 use Sunra\PhpSimple\HtmlDomParser; // For manipulating pages, eg, customising the dashboard
@@ -1553,7 +1552,7 @@ class CtrlController extends Controller
 	    ]);
 	    */
 	    
-	    Debugbar::disable();
+	    \Debugbar::disable(); // Will this kick off if we've not installed DebugBar on the main site? We need it so that we can disable it when returning Froala AJAX 
 
 	    $response = new \StdClass;
 
