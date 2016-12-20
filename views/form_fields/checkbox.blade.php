187eeebd9e1508559f4010c501ae6390835d84a0
Chris Gibson <chris@sevenpointsix.com>
2016-12-20 16:36:17 +0000

    allow tips to be displayed against checkbox fields

--- views/form_fields/checkbox.blade.php
+++ views/form_fields/checkbox.blade.php
@@ -3,6 +3,9 @@
 <div class="form-group">
 	<input type="checkbox" id="{{ $field['id'] }}"  name="{{ $field['name'] }}" value="1" data-toggle="checkbox-x" data-three-state="false" data-size="lg" @if ($field['value']) checked="checked" @endif>
 	<label for="{{ $field['id'] }}" class="cbx-label"><strong>{{ $field['label'] }}</strong></label>
+    @if (!empty($field['tip']))
+    <p class="help-block">{{ $field['tip'] }}</p>
+    @endif
 </div>
 
 {{-- This allows us to push some JS to the JS stack only once; it's a hack, but appears to work --}}
