4a3e3836b9e2961c401fd0bded89a980a1ba2392
Chris Gibson <chris@phoenixdigital.agency>
2017-02-07 10:14:26 +0000

    migration to add flags to ctrl classes

---
+++ database/migrations/2017_02_07_101051_add_flags_to_ctrl_classes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFlagsToCtrlClasses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('ctrl_classes', function (Blueprint $table) {
+            $table->enum('flags', [])->nullable(); // *** SEE BELOW
+        });
+
+        // A good solution to creating SET columns, from http://laravel.io/forum/06-18-2014-what-is-the-mysql-datatype-set-equivalent-in-laravel-schema
+        DB::statement("ALTER TABLE `ctrl_classes` CHANGE `flags` `flags` SET('dashboard');");
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ctrl_classes', function (Blueprint $table) {
+           $table->dropColumn('flags');
+        });
+    }
+}
