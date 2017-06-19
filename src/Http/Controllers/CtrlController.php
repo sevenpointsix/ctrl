74fbf31f1905bcf773953e5fcbdb57c7a6279707
Chris Gibson <chris@phoenixdigital.agency>
2017-06-19 12:41:42 +0100

    Fixing storage paths

--- src/Http/Controllers/CtrlController.php
+++ src/Http/Controllers/CtrlController.php
@@ -2452,9 +2452,12 @@ class CtrlController extends Controller
 		if (\App::VERSION() >= 5.4) {
 			/**
 			 * Storage images with a hash, preserve original filename for files
+			 * We'll store both in the public folder though, as we're likely to need direct
+			 * access at some point -- especially files, but images as well, as we
+			 * might not always run them through Intervention or whatever.
 			 */
 			if ($request->type == 'image') {
-				 $path = $request->file($fieldName)->store('public/images');
+				 $path = $request->file($fieldName)->store('images','public');
 			}
 			else if ($request->type == 'file') {
 				$fileName = $request->file($fieldName)->getClientOriginalName();
@@ -2485,12 +2488,6 @@ class CtrlController extends Controller
 				    } while (Storage::exists($fileName));
 				}
 				 */
-				/**
-				 * Note that we use the public disk to save files in storage/app/public
-				 * this is because we'll likely want to download files directly
-				 * (where as images are always passed through intervention or similar)
-				 * @var [type]
-				 */
 				$path = $request->file($fieldName)->storeAs(
 				    'files', $fileName, 'public'
 				);
@@ -2503,7 +2500,7 @@ class CtrlController extends Controller
 			 * whereas really, I think it makes more sense to store the actual path we'd use; eg, /storage/images
 			 * So, swap out the /public for /storage. This is all a bit flaky and may well need revisiting.
 			 */
-			$path = preg_replace('/^public\//', 'storage/', $path);
+			// $path = preg_replace('/^public\//', 'storage/', $path);
 		}
 		else {
 			/**
