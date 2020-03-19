2a6b1c1b8c157405e7c0b7722c71559c492f7d91
Chris Gibson <chris@phoenixdigital.agency>
2020-03-19 23:02:39 +0000

    adding froala v3 woot

--- views/form_fields/froala.blade.php
+++ views/form_fields/froala.blade.php
@@ -9,20 +9,26 @@
 @if (empty($GLOBALS['push_froala_js']))
 	@push('js')
 		<!-- Include JS files. -->
-		<script type="text/javascript" src="{{ asset('assets/vendor/ctrl/vendor/froala/js/froala_editor.min.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('assets/vendor/ctrl/vendor/froala3/js/froala_editor.pkgd.min.js') }}"></script>
+
 		{{-- New approach for 2.5+
 		<script>
 		  $.FroalaEditor.DEFAULTS.key = '{{ env('FROALA_KEY','') }}';
 		</script>
 		--}}
+		{{-- Not needed for 3? We pass the key into the initialisation I think?
 		<script id="fr-fek">try{(function (k){localStorage.FEK=k;t=document.getElementById('fr-fek');t.parentNode.removeChild(t);})('{{ env('FROALA_KEY','') }}')}catch(e){}</script>
+		--}}
 
 		<!-- Include Code Mirror. -->
+		{{-- Does anyone use Code Mirror? It's the HTML/code view AFAIK. Drop it for now:
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/mode/xml/xml.min.js"></script>
+		--}}
 
 		<!-- Include Plugins. -->
 		{{-- TODO: do we really need all these plugins?! Just pick the ones we're using. See: https://www.froala.com/wysiwyg-editor/docs/plugins --}}
+		{{-- Surely we don't need ANY of these. Maybe image? file? TBC
 		<script type="text/javascript" src="{{ asset('assets/vendor/ctrl/vendor/froala/js/plugins/align.min.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('assets/vendor/ctrl/vendor/froala/js/plugins/char_counter.min.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('assets/vendor/ctrl/vendor/froala/js/plugins/code_beautifier.min.js') }}"></script>
@@ -48,6 +54,7 @@
 		<script type="text/javascript" src="{{ asset('assets/vendor/ctrl/vendor/froala/js/plugins/save.min.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('assets/vendor/ctrl/vendor/froala/js/plugins/url.min.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('assets/vendor/ctrl/vendor/froala/js/plugins/video.min.js') }}"></script>
+		--}}
 	@endpush
 	<?php $GLOBALS['push_froala_js'] = true; ?>
 @endif
@@ -56,7 +63,10 @@
 			<!-- Initialize the editor. -->
 		<script>
 		  $(function() {
-		      $('#{{ $field['id'] }}').froalaEditor({
+		      // $('#{{ $field['id'] }}').froalaEditor({
+				  // v3:
+			var editor = new FroalaEditor('#{{ $field['id'] }}', {
+				key: "{{ env('FROALA_KEY','') }}",
 		      	/* Should be feasible to customise this on a per-field basis? */
 		      	toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent','|',  'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable'],
 		      	/* Select from:
@@ -87,13 +97,16 @@
 @if (empty($GLOBALS['push_froala_css']))
 	@push('css')
 		<!-- Include Editor style. -->
-		<link href="{{ asset('assets/vendor/ctrl/vendor/froala/css/froala_editor.min.css') }}" rel="stylesheet" type="text/css" />
-		<link href="{{ asset('assets/vendor/ctrl/vendor/froala/css/froala_style.min.css') }}" rel="stylesheet" type="text/css" />
+		<link href="{{ asset('assets/vendor/ctrl/vendor/froala3/css/froala_editor.pkgd.min.css') }}" rel="stylesheet" type="text/css" />
+		<link href="{{ asset('assets/vendor/ctrl/vendor/froala3/css/froala_style.min.css') }}" rel="stylesheet" type="text/css" />
 
+		{{-- Does anyone use Code Mirror? It's the HTML/code view AFAIK. Drop it for now:
 		<!-- Include Code Mirror style -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.css">
+		--}}
 
 		<!-- Include Editor Plugins style. -->
+		{{-- Drop all these for now as well:
 		<link rel="stylesheet" href="{{ asset('assets/vendor/ctrl/vendor/froala/css/plugins/char_counter.css') }}">
 		<link rel="stylesheet" href="{{ asset('assets/vendor/ctrl/vendor/froala/css/plugins/code_view.css') }}">
 		<link rel="stylesheet" href="{{ asset('assets/vendor/ctrl/vendor/froala/css/plugins/colors.css') }}">
@@ -106,6 +119,7 @@
 		<link rel="stylesheet" href="{{ asset('assets/vendor/ctrl/vendor/froala/css/plugins/quick_insert.css') }}">
 		<link rel="stylesheet" href="{{ asset('assets/vendor/ctrl/vendor/froala/css/plugins/table.css') }}">
 		<link rel="stylesheet" href="{{ asset('assets/vendor/ctrl/vendor/froala/css/plugins/video.css') }}">
+		--}}
 	@endpush
 	<?php $GLOBALS['push_froala_css'] = true; ?>
 @endif
