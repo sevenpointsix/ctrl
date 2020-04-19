14b3d49b8ec48e35c5df42e3219e30f687a8dd14
Chris Gibson <chris@phoenixdigital.agency>
2020-04-19 12:41:41 +0100

    fixing error messages on validation

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,17 +1,17 @@
-$(document).ready(function() { 
+$(document).ready(function() {
 
   $('form.ajax').on('submit',function(e) {
-    
+
     var submit_button = $(this).find('button[type=submit][data-loading-text]');
     if (submit_button.length > 0) {
       var submit_button_original_text = submit_button.html();
-      submit_button.html(submit_button.attr('data-loading-text'));  
+      submit_button.html(submit_button.attr('data-loading-text'));
     }
 
     $.ajaxSetup({
         headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
     });
-    
+
     e.preventDefault(e);
 
     $.ajax({
@@ -19,29 +19,29 @@ $(document).ready(function() {
       url:$(this).attr('action'),
       data:$(this).serialize(),
       dataType: 'json',
-      success: function(responseText, statusText, xhr, $form)  { 
-        redirect = responseText.redirect;  
+      success: function(responseText, statusText, xhr, $form)  {
+        redirect = responseText.redirect;
         window.location.replace(redirect);
         // What's the best way to add a .notify here?
       },
-      error: function(jqXHR, textStatus, errorThrown ) { 
+      error: function(jqXHR, textStatus, errorThrown ) {
 
         if (jqXHR.status == 422 || jqXHR.status == 500) { // 422 is the status code Laravel returns when validation fails:
-          
+
           var error_messages = new Array();
 
           if (jqXHR.status == 422) { // Validation error
             response = $.parseJSON(jqXHR.responseText);
-            $.each(response, function(field, error) {              
-              // Field [name=field] now has the error error           
-              $('[name="'+field+'"]').parents('div.form-group').addClass('has-error');                          
+            $.each(response.errors, function(field, error) {
+              // Field [name=field] now has the error error
+              $('[name="'+field+'"]').parents('div.form-group').addClass('has-error');
               error_messages.push(error);
-            }); 
+            });
           }
           else if (jqXHR.status == 500) { // Server error
-            //console.log(jqXHR.responseText);              
+            //console.log(jqXHR.responseText);
             error_messages.push('An error has occurred; please contact support.');
-          }  
+          }
 
           // We now use Handlebars here, to load the error HTML.
           var errors = error_messages.join('<br>');
@@ -55,10 +55,10 @@ $(document).ready(function() {
             scrollTop: $('#messages').offset().top - 100
           }, 'fast');
 
-          if (submit_button.length > 0) {            
-            submit_button.html(submit_button_original_text);  
+          if (submit_button.length > 0) {
+            submit_button.html(submit_button_original_text);
           }
-          
+
         }
         else {
           // Should no longer here, as we catch 500s above...
@@ -67,13 +67,13 @@ $(document).ready(function() {
             console.log(errorThrown+': '+response.error.exception);
           }
           else {
-            console.log('Error');      
+            console.log('Error');
           }
-          if (submit_button.length > 0) {            
-            submit_button.html(submit_button_original_text);  
+          if (submit_button.length > 0) {
+            submit_button.html(submit_button_original_text);
           }
         }
-      }     
+      }
     });
   });
 });
